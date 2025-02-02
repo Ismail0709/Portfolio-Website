d795bdf48a72e054781e46898908d352a3ce7662
Ismail0709 <ismailbajwa2003@gmail.com>
2025-02-03 01:27:08 +0500

    refactoring 03

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -1,29 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { sanityClient } from "@/sanity";
 import { PageInfo } from "@/typings";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 
-const query = groq`
-  *[_type == "pageInfo"][0]
-`;
+const query = groq` *[_type == "pageInfo"][0] `;
 
-type Data = {
-    pageInfo?: PageInfo; 
-    message?: string;    
-};
+type Data = { pageInfo?: PageInfo; message?: string; };
 
 export default async function handler(
-    _req: NextApiRequest,  
-    res: NextApiResponse<Data>
+  _req: NextApiRequest,   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  res: NextApiResponse<Data>
 ) {
-    try {
-        const pageInfo: PageInfo = await sanityClient.fetch(query);
-        res.status(200).json({ pageInfo });
-    } catch (error) {
-        console.error("Error fetching page info:", error);
-
-        res.status(500).json({ message: "An error occurred while fetching page info" });
-    }
+  try {
+    const pageInfo: PageInfo = await sanityClient.fetch(query);
+    res.status(200).json({ pageInfo });
+  } catch (error) {
+    console.error("Error fetching page info:", error); // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    res.status(500).json({ message: "An error occurred while fetching page info" });
+  }
 }
-/* eslint-enable @typescript-eslint/no-unused-vars */
